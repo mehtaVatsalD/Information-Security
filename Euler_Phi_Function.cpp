d300fea9355e12ad83f379e428a69d2badcd0074
jigarprajapati12345666 <jigarprajapati12345666@gmail.com>
2018-04-06 19:48:06 +0530

    Add files via upload

---
+++ Euler_Phi_Function.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+int phi(int n)
+{
+    int m = n,i,a[100]={0},j=0;
+    for(i=2 ;i<m/2 ;i++)
+    {
+        while(n % i == 0 && n>0)
+        {
+            a[j] = i;
+            n = n/i;
+            j++;
+        }
+        if(n == 0)
+        {
+            break;
+        }
+    }
+    long long answer=1;
+    int c =1,k;
+    for(int i=0 ;i<j ;i++)
+    {
+         k = a[i];
+        c = 1;
+        while(a[i] == a[i+1])
+        {
+            c++;
+            i++;
+
+        }
+        //cout << c << " " << k << endl;
+        answer *= (pow(k,c)-pow(k,c-1));
+        //cout << answer << endl;
+    }
+    return answer;
+}
+int main()
+{
+    int n,answer;
+    cin >> n;
+    answer = phi(n);
+    cout << "phi(" << n << ")" " = " << answer << endl;
+    return 0;
+}
